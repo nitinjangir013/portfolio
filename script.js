48b8937e37e8eb322a0675cb17f38015c618a6d5
Nitin Jangir <115369751+nitinjangir013@users.noreply.github.com>
2024-04-14 23:30:51 +0530

    Update theme color

--- script.js
+++ script.js
@@ -14,17 +14,42 @@ window.onscroll = () => {
 };
 
 // Dark Mode / light mode
+// let darkmode = document.querySelector("#darkmode");
+
+// darkmode.onclick = () => {
+//   if (darkmode.classList.contains("bx-moon")) {
+//     darkmode.classList.replace("bx-moon", "bx-sun");
+//     document.body.classList.add("active");
+//   } else {
+//     darkmode.classList.replace("bx-sun", "bx-moon");
+//     document.body.classList.remove("active");
+//   }
+// };
+
+
 let darkmode = document.querySelector("#darkmode");
+let mode = localStorage.getItem("mode");
+
+if (mode === "dark") {
+  darkmode.classList.replace("bx-moon", "bx-sun");
+  document.body.classList.add("active");
+} else {
+  darkmode.classList.replace("bx-sun", "bx-moon");
+  document.body.classList.remove("active");
+}
 
 darkmode.onclick = () => {
   if (darkmode.classList.contains("bx-moon")) {
     darkmode.classList.replace("bx-moon", "bx-sun");
     document.body.classList.add("active");
+    localStorage.setItem("mode", "dark"); // Save dark mode preference
   } else {
     darkmode.classList.replace("bx-sun", "bx-moon");
     document.body.classList.remove("active");
+    localStorage.setItem("mode", "light"); // Save light mode preference
   }
 };
+
 
 
 document.addEventListener('submit', function(event) {
