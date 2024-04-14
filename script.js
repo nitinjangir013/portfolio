c80f71f538183ebcecad05830f4b0d9b07f229a0
Nitin Jangir <115369751+nitinjangir013@users.noreply.github.com>
2024-04-14 22:57:29 +0530

    form data set

--- script.js
+++ script.js
@@ -27,14 +27,19 @@ darkmode.onclick = () => {
 };
 
 
-$(document).on('submit', '#contactForm',function(event) {
-  event.preventDefault();
-  var name = $('#name').val();
-  var email = $('#email').val();
-  var message = $('#message').val();
-  var textMessage = 'Name: ' + name + '\nEmail: ' + email + '\nMessage: ' + message;
-  
-  var phoneNumber = '+917357819424';
-  var whatsappLink = 'https://wa.me/' + phoneNumber + '?text=' + encodeURIComponent(textMessage);
-  window.location.href = whatsappLink;
+document.addEventListener('submit', function(event) {
+  if (event.target.id === 'contactForm') {
+    event.preventDefault();
+    
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    
+    var textMessage = 'Name: ' + name + '\nEmail: ' + email + '\nMessage: ' + message;
+    
+    var phoneNumber = '+917357819424';
+    var whatsappLink = 'https://wa.me/' + phoneNumber + '?text=' + encodeURIComponent(textMessage);
+    
+    window.location.href = whatsappLink;
+  }
 });
