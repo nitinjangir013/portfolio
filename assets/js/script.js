af6576726309634242883b801ca97459732d0807
Nitin Jangir <115369751+nitinjangir013@users.noreply.github.com>
2025-01-16 17:18:27 +0530

    Create New Version
    Create New Version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,34 @@
-let header = document.querySelector("header");
-let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
-
-window.addEventListener("scroll", () => {
-  header.classList.toggle("shadow", window.scrollY > 0);
+document.addEventListener('submit', function(event) {
+	if (event.target.id === 'contactForm') {
+		event.preventDefault();
+		
+		var name = document.getElementById('name').value;
+		var email = document.getElementById('email').value;
+		var message = document.getElementById('message').value;
+		
+		var textMessage = 'Name: ' + name + '\nEmail: ' + email + '\nMessage: ' + message;
+		
+		var phoneNumber = '+917357819424';
+		var whatsappLink = 'https://wa.me/' + phoneNumber + '?text=' + encodeURIComponent(textMessage);
+		
+		window.location.href = whatsappLink;
+	}
 });
 
-menu.onclick = () => {
-  navbar.classList.toggle("active");
-};
-window.onscroll = () => {
-  navbar.classList.remove("active");
-};
+$(document).ready(function(){
+	$(".nav-link").on('click', function(event) {
 
-// Dark Mode / light mode
-// let darkmode = document.querySelector("#darkmode");
+    	if (this.hash !== "") {
 
-// darkmode.onclick = () => {
-//   if (darkmode.classList.contains("bx-moon")) {
-//     darkmode.classList.replace("bx-moon", "bx-sun");
-//     document.body.classList.add("active");
-//   } else {
-//     darkmode.classList.replace("bx-sun", "bx-moon");
-//     document.body.classList.remove("active");
-//   }
-// };
+			event.preventDefault();
 
+			var hash = this.hash;
 
-let darkmode = document.querySelector("#darkmode");
-let mode = localStorage.getItem("mode");
-
-if (mode === "dark") {
-  darkmode.classList.replace("bx-moon", "bx-sun");
-  document.body.classList.add("active");
-} else {
-  darkmode.classList.replace("bx-sun", "bx-moon");
-  document.body.classList.remove("active");
-}
-
-darkmode.onclick = () => {
-  if (darkmode.classList.contains("bx-moon")) {
-    darkmode.classList.replace("bx-moon", "bx-sun");
-    document.body.classList.add("active");
-    localStorage.setItem("mode", "dark"); // Save dark mode preference
-  } else {
-    darkmode.classList.replace("bx-sun", "bx-moon");
-    document.body.classList.remove("active");
-    localStorage.setItem("mode", "light"); // Save light mode preference
-  }
-};
-
-
-
-document.addEventListener('submit', function(event) {
-  if (event.target.id === 'contactForm') {
-    event.preventDefault();
-    
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
-    
-    var textMessage = 'Name: ' + name + '\nEmail: ' + email + '\nMessage: ' + message;
-    
-    var phoneNumber = '+917357819424';
-    var whatsappLink = 'https://wa.me/' + phoneNumber + '?text=' + encodeURIComponent(textMessage);
-    
-    window.location.href = whatsappLink;
-  }
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, function(){
+				window.location.hash = hash;
+			});
+      	} 
+    });
 });
